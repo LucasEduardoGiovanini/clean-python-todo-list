f1e31dc0e234581f570c49b553da8095a979b37b
LucasEduardoGiovanini <39911940+LucasEduardoGiovanini@users.noreply.github.com>
2020-04-14 11:40:03 -0300

    Creating a use case package for call in the tests.

---
+++ domain/use_cases.py
@@ -0,0 +1,42 @@
+from .entities.task import Task
+from .entities.todolist import TodoList
+
+
+def Create_list(listname: str):
+    if listname == str:
+        list1 = TodoList(listname)
+        return True
+    return False
+
+
+def add_task(task_list: TodoList, task: Task):
+    task_list.addTask(task)
+    return True
+
+
+def remove_task(task_list: TodoList, task: Task):
+    if task_list.tasks.index(task):
+        task_list.removeTask(task)
+        return True
+    return False
+
+
+def complete_task(task_list: TodoList, task: Task):
+    if task_list.tasks.index(task):
+        task.completeTask()
+        return True
+    return False
+
+
+def undo_task(task_list: TodoList, task: Task):
+    if task_list.tasks.index(task):
+        task.undoTask()
+        return True
+    return False
+
+
+def order_task(task_list: TodoList, task_to_be_relocated: Task, task_that_give_position: Task):
+    if task_list.tasks.index(task_to_be_relocated) and task_list.tasks.index(task_that_give_position):
+        task_list.orderTasks(task_to_be_relocated, task_that_give_position)
+        return True
+    return False
