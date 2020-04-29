b92d2d45b09664aaecaa2ee9b7177f554efe409a
LucasEduardoGiovanini <39911940+LucasEduardoGiovanini@users.noreply.github.com>
2020-04-29 11:48:23 -0300

    removing all dependences about the id's.
    if run, this commit will remove all id parameter  from the functions create_todo_list and create_task, in package todo_list_repository. As the id is generate in database now, it is no longer needed like a parameter here.

--- repositories/todo_list_repository.py
+++ repositories/todo_list_repository.py
@@ -11,14 +11,14 @@ class TodoListRepository:
                                           db='dbTodoList',
                                           cursorclass=pymysql.cursors.DictCursor)
 
-    def create_todo_list(self, email: str, id_code: str, list_name: str):
+    def create_todo_list(self, email: str, list_name: str):
         cursor = self.connection.cursor()
-        arguments = (id_code, email, list_name)
-        cursor.execute("INSERT INTO tbTodoList(todolist_id, email, todo_name) VALUES (%s, %s, %s)", arguments)
+        arguments = (email, list_name)
+        cursor.execute("INSERT INTO tbTodoList(email, todo_name) VALUES (%s, %s)", arguments)
         self.connection.commit()
-        cursor.execute("SELECT * FROM tbTodoList WHERE todolist_id = %s", arguments[0])
+        cursor.execute("SELECT * FROM tbTodoList WHERE email = %s AND todo_name = %s", arguments)
         result_dictionary = cursor.fetchone()
-        return TodoList(result_dictionary['todolist_id'], result_dictionary['todo_name'])
+        return TodoList(result_dictionary['todo_name'])
 
     def get_the_next_free_position(self, id_list: str):# pego a maior posição da fila
         cursor = self.connection.cursor()
@@ -27,11 +27,11 @@ class TodoListRepository:
         result = cursor.fetchone()
         return result['max(queue_position)']+1 if result['max(queue_position)'] is not None else 0# se não tiver nenhuma task, retorno a primeira posição disponível.
 
-    def create_task(self, id_task: str, id_list: str, name: str, description: str, completed: bool, priority: int, queue_position: int):
+    def create_task(self, id_list: str, name: str, description: str, completed: bool, priority: int, queue_position: int):
         cursor = self.connection.cursor()
-        arguments = (id_task, id_list, name, description, completed, priority, queue_position)
-        cursor.execute("INSERT INTO tbTask(task_id,todolist_id,task_name,descripton,completed,priority,queue_position) VALUES (%s, %s, %s,%s,%s,%s,%s)", arguments)
+        arguments = (id_list, name, description, completed, priority, queue_position)
+        cursor.execute("INSERT INTO tbTask(todolist_id,task_name,descripton,completed,priority,queue_position) VALUES (%s, %s,%s,%s,%s,%s)", arguments)
         self.connection.commit()
-        cursor.execute("SELECT * FROM tbTask WHERE task_id = %s", arguments[0])
+        cursor.execute("SELECT * FROM tbTask WHERE todolist_id = %s AND task_name = %s AND descripton = %s AND completed = %s AND priority = %s AND queue_position = %s", arguments)
         result_dictionary = cursor.fetchone()
-        return TodoList().create_task(result_dictionary['task_id'], result_dictionary['task_name'], result_dictionary['descripton'], result_dictionary['completed'], result_dictionary['priority'])
+        return TodoList().create_task(result_dictionary['task_name'], result_dictionary['descripton'], bool(result_dictionary['completed']), int(result_dictionary['priority']))
